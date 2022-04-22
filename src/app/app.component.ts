fe95cc5f50848faf2f21dabde020dca390a8f1cd
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2022-04-22 08:53:57 +0000

    UI done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'trial';
+  color:string ="";
+  Changecolor(color:string){
+    console.log(color);
+    this.color = color;
+  }
 }
